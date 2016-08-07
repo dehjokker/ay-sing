721f82e207bba339cbbd56a54451563f8d9f89a1
Kyle Roach <kroach.work@gmail.com>
2016-08-06 23:05:56 -0400

    Completed numpad

--- app/pages/numpad/numpad.ts
+++ app/pages/numpad/numpad.ts
@@ -1,11 +1,41 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import { AlertController, AlertOptions } from 'ionic-angular/components';
 
 @Component({
   templateUrl: 'build/pages/numpad/numpad.html'
 })
 export class NumpadPage {
-  constructor(private navCtrl: NavController) {
+  number: string;
 
+  constructor(private navCtrl: NavController, private alertController: AlertController) {
+    this.number = '';
+  }
+
+  enterDigit(digit) {
+    if (this.number.length < 3) {
+      this.number += digit;
+    }
+  }
+
+  clearInput() {
+    this.number = "";
+  }
+
+  backspace() {
+    this.number = this.number.slice(0, this.number.length - 1);
+  }
+
+  go() {
+    if (Number(this.number) > 214) {
+      this.clearInput();
+      this.alertController.create({
+        title: 'Sorry',
+        subTitle: 'The song could not be found.',
+        buttons: ['OK']
+      }).present();
+    } else {
+      // Lookup hymn
+    }
   }
 }
